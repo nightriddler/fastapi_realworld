657132aac021e9cbe9c17101a3905f65ed37c817
Michail Eisenberg <75097575+nightriddler@users.noreply.github.com>
2021-12-09 15:56:19 +0300

    minor changes

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -25,7 +25,9 @@ DB_TEST_NAME = os.environ.get("DB_TEST_NAME")
 
 # SQLALCHEMY_DATABASE_URL = f"{DB_TEST_DIALECT}+{DB_TEST_DRIVER}://{DB_TEST_USERNAME}:{DB_TEST_PASSWORD}@{DB_TEST_HOST}/{DB_TEST_NAME}"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+)
 print(SQLALCHEMY_DATABASE_URL)
 
 
